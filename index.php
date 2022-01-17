0b5544faf87329109734191c7278c672883edad9
Janani <vsvprema@gmail.com>
2022-01-17 11:23:40 +0530

    Added: Create a ToDoList using CRUD

---
+++ index.php
@@ -0,0 +1,105 @@
+<?php
+// initialize errors variable
+$update = false;
+$errors = "";
+// connect to database
+$db = mysqli_connect("localhost", "root", "", "todo");
+// insert a quote if submit button is clicked
+if (isset($_POST['submit'])) {
+    if (empty($_POST['task'])) {
+        $errors = "You must fill in the task";
+    } else {
+        $task = $_POST['task'];
+        $sql = "INSERT INTO tasks (task) VALUES ('$task')";
+        mysqli_query($db, $sql);
+        header('location: index.php');
+    }
+}
+// edit task
+if (isset($_GET['edit_task'])) {
+    $id = $_GET['edit_task'];
+    $update = true;
+    $result = mysqli_query($db, "SELECT * FROM tasks WHERE id='$id'");
+    $row = mysqli_fetch_assoc($result);
+    $task = $row['task'];
+}
+//update task
+if (isset($_POST['update'])) {
+    echo "Inside Update Condition: " . $_POST['update'];
+    $id = $_POST['id'];
+    $task = $_POST['task'];
+    $sql = "UPDATE tasks SET task='$task' WHERE id='$id'";
+    $result = mysqli_query($db, $sql);
+    if ($result) {
+        header("location: index.php");
+    }
+}
+// delete task
+if (isset($_GET['del_task'])) {
+    $id = $_GET['del_task'];
+    mysqli_query($db, "DELETE FROM tasks WHERE id=" . $id);
+    header('location: index.php');
+}
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>ToDo List Application PHP and MySQL</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div class="heading">
+        <h2 style="font-style: 'Hervetica';">ToDo List Application PHP and MySQL database</h2>
+    </div>
+    <form method="post" action="index.php" class="input_form">
+        <?php if (isset($errors)) { ?>
+            <p><?php echo $errors; ?></p>
+        <?php } ?>
+        <input type="hidden" name="id" value="<?php echo $id; ?>">
+        <input type="text" name="task" class="task_input">
+        <?php
+        if ($update == true) :
+        ?>
+            <button type="submit" name="update" id="add_btn" class="add_btn">Update</button>
+        <?php else : ?>
+            <button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
+        <?php endif; ?>
+
+    </form>
+    <table>
+        <thead>
+            <tr>
+                <th>S.No</th>
+                <th>Tasks</th>
+                <th style="width: 200px;">Action</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php
+            // select all tasks if page is visited or refreshed
+            $result = mysqli_query($db, "SELECT * FROM tasks");
+            $i = 1;
+            while ($row = mysqli_fetch_assoc($result)) {
+                $id = $row['id'];
+                $task = $row['task'];
+            ?>
+                <tr>
+
+                    <td class="serialNumber"> <?php echo $i ?> </td>
+                    <td class="task"> <?php echo $task ?> </td>
+                    <td class="edit_delete">
+                        <a href="index.php?edit_task=<?php echo $id ?>">edit</a>
+                        <a href="index.php?del_task=<?php echo $id ?>">x</a>
+                    </td>
+                </tr>
+            <?php $i++;
+            } ?>
+        </tbody>
+    </table>
+</body>
+
+</html>
