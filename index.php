54eed127cb32082c5daa307b1158f69cc1d0ca61
Janani <vsvprema@gmail.com>
2022-01-22 08:53:35 +0530

    Added: create a ToDoList using CRUD

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
-// initialize errors variable
+
+/**
+ * Author : Janani VijayaKumar
+ * Date : 17-01-2022
+ * Desc : Create a To do list using CRUD in php
+ */
+
+// initialize errors variable and update
 $update = false;
 $errors = "";
 // connect to database
-$db = mysqli_connect("localhost", "root", "", "todo");
+$db = mysqli_connect("localhost:3307", "root", "", "todo");
 // insert a quote if submit button is clicked
 if (isset($_POST['submit'])) {
     if (empty($_POST['task'])) {
@@ -11,6 +18,16 @@ if (isset($_POST['submit'])) {
     } else {
         $task = $_POST['task'];
         $sql = "INSERT INTO tasks (task) VALUES ('$task')";
+        mysqli_query($db, $sql);
+        header('location: index.php');
+    }
+}
+if (isset($_POST['edit'])) {
+    if (empty($_POST['task'])) {
+        $errors = "You must fill in the task";
+    } else {
+        $task = $_POST['task'];
+        $sql = "SELECT * FROM tasks WHERE id='$id'";
         mysqli_query($db, $sql);
         header('location: index.php');
     }
@@ -34,6 +51,7 @@ if (isset($_POST['update'])) {
         header("location: index.php");
     }
 }
+
 // delete task
 if (isset($_GET['del_task'])) {
     $id = $_GET['del_task'];
@@ -54,21 +72,25 @@ if (isset($_GET['del_task'])) {
     <div class="heading">
         <h2 style="font-style: 'Hervetica';">ToDo List Application PHP and MySQL database</h2>
     </div>
-    <form method="post" action="index.php" class="input_form">
-        <?php if (isset($errors)) { ?>
-            <p><?php echo $errors; ?></p>
-        <?php } ?>
-        <input type="hidden" name="id" value="<?php echo $id; ?>">
-        <input type="text" name="task" class="task_input">
-        <?php
-        if ($update == true) :
-        ?>
-            <button type="submit" name="update" id="add_btn" class="add_btn">Update</button>
-        <?php else : ?>
-            <button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
-        <?php endif; ?>
+    <center>
+        <form method="post" action="index.php" class="input_form">
+            <?php if (isset($errors)) { ?>
+                <p><?php echo $errors; ?></p>
+            <?php } ?>
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
 
-    </form>
+            <?php
+            if ($update == true) :
+            ?>
+                <input type="text" name="task" class="task_input" value="<?php echo $task; ?>">
+                <button type="submit" name="update" id="add_btn" class="add_btn">Update</button>
+            <?php else : ?>
+                <input type="text" name="task" class="task_input">
+                <button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
+            <?php endif; ?>
+
+        </form>
+    </center>
     <table>
         <thead>
             <tr>
@@ -79,25 +101,26 @@ if (isset($_GET['del_task'])) {
         </thead>
 
         <tbody>
-            <?php
-            // select all tasks if page is visited or refreshed
-            $result = mysqli_query($db, "SELECT * FROM tasks");
-            $i = 1;
-            while ($row = mysqli_fetch_assoc($result)) {
-                $id = $row['id'];
-                $task = $row['task'];
-            ?>
-                <tr>
-
-                    <td class="serialNumber"> <?php echo $i ?> </td>
-                    <td class="task"> <?php echo $task ?> </td>
-                    <td class="edit_delete">
-                        <a href="index.php?edit_task=<?php echo $id ?>">edit</a>
-                        <a href="index.php?del_task=<?php echo $id ?>">x</a>
-                    </td>
-                </tr>
-            <?php $i++;
-            } ?>
+            <center>
+                <?php
+                // select all tasks if page is visited or refreshed
+                $result = mysqli_query($db, "SELECT * FROM tasks");
+                $i = 1;
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $id = $row['id'];
+                    $task = $row['task'];
+                ?>
+                    <tr>
+                        <td class="serialNumber"> <?php echo $i ?> </td>
+                        <td class="task"> <?php echo $task ?> </td>
+                        <td class="edit_delete">
+                            <a href="index.php?edit_task=<?php echo $id ?>">edit</a>
+                            <a href="index.php?del_task=<?php echo $id ?>">x</a>
+                        </td>
+                    </tr>
+            </center>
+        <?php $i++;
+                } ?>
         </tbody>
     </table>
 </body>
